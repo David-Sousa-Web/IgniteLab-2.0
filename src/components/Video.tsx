1afd7778f39237e436c2e8eb2ae31e9d3c2d89f8
David Sousa <david01sousa@Hotmail.com>
2022-08-12 21:21:39 -0300

    Adicionando o Footer

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,6 +3,7 @@ import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-reac
 
 import '@vime/core/themes/default.css';
 import { gql, useQuery } from "@apollo/client";
+import { LogoRocket } from "./LogoRocket";
 
 const GET_LESSON_BY_SLUG_QUERY = gql`
   query GetLessonBySlug($slug: String) {
@@ -134,7 +135,19 @@ export function Video(props: VideoProps){
           </a>
         </div>
       </div>
-
+      <footer className="flex py-6 items-center justify-between border-t-[1px] border-gray-300 w-[90%] m-auto mt-2">
+        <div className="flex gap-4 items-center">
+          <LogoRocket />
+          <span className="text-gray-300">
+            Rocketseat - Todos os direitos reservados
+          </span>
+        </div>
+        <div className="flex">
+          <span className="text-gray-300">
+            Políticas de privacidade
+          </span>
+        </div>
+      </footer>
     </div>
   )
 }
